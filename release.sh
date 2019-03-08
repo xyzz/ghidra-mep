655f704c5919ae341bf6be428a36c3a6a629547b
Ilya Zhuravlev <whatever@xyz.is>
2019-03-08 06:36:54 -0800

    Add release script

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+temp_dir=$(mktemp -d)
+trap "rm -rf $temp_dir" 0 2 3 15
+
+pushd $temp_dir
+
+cp -r $DIR mep
+rm -rf mep/.git* mep/README.md mep/release.sh mep/mep.zip
+cd mep/data/languages
+sleigh -a
+cd ../../..
+zip -r mep.zip mep
+cp mep.zip $DIR
+
+popd
+
+echo ""
+ls -la mep.zip
